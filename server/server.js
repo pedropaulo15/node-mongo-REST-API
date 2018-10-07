8c29b6efef8aed12d2b838c8bb615df0eab47eb1
Pedro <pedropaulo15@gmail.com>
2018-10-07 20:39:02 +0100

    Using mongosse to create and insert data into user model.

--- server/server.js
+++ server/server.js
@@ -4,27 +4,39 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 // Defining a Todo model
-const Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
+// const Todo = mongoose.model('Todo', {
+//   text: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   },
+//   completedAt: {
+//     type: Number,
+//     default: null
+//   }
+// });
+
+// Creating a new role/document
+// const newTodo = new Todo({ text: '   Edit the video.   '});
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   }
 });
 
-// Creating a new role/document
-const newTodo = new Todo({
-  text: 'Cook dinner',
-  completed: true,
-  completedAt: 123
-});
+const newUser = new User({ email: '      '});
 
-newTodo.save().then((doc) => {
-  console.log(`Saved todo ${doc}`)
+newUser.save().then((doc) => {
+  console.log(`Saved user ${doc}`)
 }, (e) => {
-  console.log('Unable to save todo.');
+  console.log('Unable to save todo.', e);
 });
