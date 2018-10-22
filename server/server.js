fbb94bd3934b19c9abc88553465b27e14b99f497
Pedro <pedropaulo15@gmail.com>
2018-10-22 21:52:20 +0100

    Add PATCH/todos/:id route.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
@@ -11,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Creating the routes
-// POST a new to do
+// POST a new todo
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -24,7 +25,7 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// GET all to dos
+// GET all todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -33,7 +34,7 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET to do by ID
+// GET todo by ID
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
 
@@ -54,7 +55,7 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-// DELETE to do by ID
+// DELETE todo by ID
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
 
@@ -73,6 +74,42 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
     return console.log('[ERROR]', err);
   });
+});
+
+// UPDATE todo
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']); // Subset of attributes that is sent
+
+  if(!ObjectId.isValid(id)){
+    res.status(404).send();
+    return console.log('ID not valid, please try again.');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime(); // Setting a timestamp for competedAt 
+  } else {
+    body.completed = false;
+    body.completedAt = null; // clena the timestamp if the todo hasn't been completed
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+      new: true // Return the object with the new values
+      }).then((todo) => {
+
+    if (!todo){
+      res.status(404).send();
+      return console.log('To do not found.');
+    }
+
+    res.send({todo});
+
+  }).catch((err) => {
+    res.status(400).send();
+    return console.log('[ERROR]', err);
+  })
 });
 
 app.listen(port, () => {
