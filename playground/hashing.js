ab21882568ce3957fd59212ffbe299f31b1b1d89
Pedro <pedropaulo15@gmail.com>
2018-11-13 00:29:21 +0000

    Playground with crypto library.

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,26 @@
 const {SHA256} = require('crypto-js');
 
-const message = 'I am user number 3'
-const hash = SHA256(message).toString();
+// const message = 'I am user number 3';
+// const hash = SHA256(message).toString();
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// const data = {
+//   id: 4
+// };
+// const token = {
+//   data: data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// token.data.id = 5;
+// token.hash =  SHA256(JSON.stringify(token.data)).toString();
+
+// const resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash){
+//   console.log('Data not changed.');
+// } else {
+//   console.log('Data changed, do not trust.')
+// }
